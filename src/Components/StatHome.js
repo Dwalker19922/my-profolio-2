355b27fb83e50317a9249690b710c479fc7bd390
Daniel Walker <dw765608@gmail.com>
2021-06-16 01:37:34 -0400

    wip

--- src/Components/StatHome.js
+++ src/Components/StatHome.js
@@ -6,17 +6,19 @@ import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import { useState} from "react";
 import homebck from "../img/hmPage.jpg"
+import profileImage from "../img/profile_pic.jpg"
 
 export default function StatHome(props) {
     const defaultValue = 5
     const maxDist = 2
+    const maxMarquee =-120
     const [slider, setSlider] = useState(false)
     const [marquee,setMarquee] = useState(-440)
     const [distance,setDistance] =useState(defaultValue)
     const transparency =  (marquee + 440)*2
     console.log(transparency)
     const runMarquee = ()=>{
-        if(marquee<-120){
+        if(marquee<maxMarquee){
 
         setTimeout(() =>{
         setMarquee(marquee+.5)
@@ -100,21 +102,48 @@ const timerFWD = ()=>{
           },
           info:{
               backgroundColor:"grey",
+              display:"flex",
+              flexDirection:"column",
+              alignItems: "center",
+              justifyContent:"center",
               padding:"10%",
               width:"60%",
-              height:"80%",
+              height:"70%",
               opacity:`${transparency}%`,
               borderRadius:"10%",
               maxHeight:"500px",
-              maxWidth:"500px",
-              marginLeft:"10%"
+              maxWidth:"400px",
+              minHeight:"520px",
+              marginLeft:"10%",
+              border:"10px solid black"
+
           },
           infoOne:{
             color: "black",
-            fontSize:"2vw"
+            fontSize:"4.5vh"
+          },
+          profilePic:{
+            backgroundImage:`url(${profileImage})`,
+            padding: "3%",
+            backgroundPosition:"center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize:"110%",
+            width:"20%",
+            height:"20%",
+            borderRadius:"50%",
+            margin:"0 auto",
+            minWidth:"100px",
+            maxWidth:"30%",
+            minHeight:"100px",
+            maxHeight:"30%",
+            marginTop:"70px"
+          },
+          para: {
+              fontSize:"1.2vh",
+              fontWeight:"bold",
+            wordSpacing:"2px"
           }
     })
-//  -440  -120
 
     const classes = styled()
     return (
@@ -126,13 +155,14 @@ const timerFWD = ()=>{
             <div className={classes.container}>
                 <div className={classes.main}>
                     <div className={classes.info}>
+                        <div className={classes.profilePic}/>
                         <p className={classes.infoOne}>welcome to my page</p>
-                        <p>Info about me blah blah blah.....</p>
-                        <p>Info about me blah blah blah.....</p>
-                        <p>Info about me blah blah blah.....</p>
-                        <p>Info about me blah blah blah.....</p>
-                        <p>Link to resume</p>
-                        <p>Link to LinkedIn</p>
+                        <p className={classes.para}>Info about me blah blah blah.....</p>
+                        <p className={classes.para}>Info about me blah blah blah.....</p>
+                        <p className={classes.para}>Info about me blah blah blah.....</p>
+                        <p className={classes.para}>Info about me blah blah blah.....</p>
+                        <p className={classes.para}>Link to resume</p>
+                        <p className={classes.para}>Link to LinkedIn</p>
                         </div>
                   
                     <div className={classes.intro}>
