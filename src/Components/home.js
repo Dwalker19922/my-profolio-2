925d03ec260cc59baacc64ca62b8f19b60502258
Daniel Walker <dw765608@gmail.com>
2021-06-16 04:16:10 -0400

    wip

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,6 +1,5 @@
 
 import {useEffect } from "react";
-import ScrollHome from "./scrollHome"
 import StatHome from "./StatHome"
 export default function HomePg(props) {
     const scroll=props.scroll
@@ -14,11 +13,11 @@ export default function HomePg(props) {
             setStat(true)
         },1000)}
     })
-    
+
     const window = props.window
     return (
  <>
- {stat===false?<ScrollHome window={window} classes={props.classes.home}/>:<StatHome window={window} classes={props.classes.home}/>}
+ <StatHome window={window} classes={props.classes.home}/>
  </>
      
     )
--- src/Components/scrollHome.js
+++
@@ -1,8 +0,0 @@
-export default function ScrollHome(props) {
-    return (
-
-        <div className={props.classes}>
-            <h1>LOADING</h1>
-        </div>
-    )
-}
